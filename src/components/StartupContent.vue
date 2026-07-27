3798726d453a181de01d070b6badfbf36b2371db
fneumann <fneumann@databay.de>
2026-07-27 17:39:15 +0200

    Change title style on startup screen

--- src/components/StartupContent.vue
+++ src/components/StartupContent.vue
@@ -7,7 +7,7 @@ import {stores} from "@/store";
   <v-main fill-height>
 
     <v-app-bar elevation="1" color="white" density="compact">
-      <p>{{ $t('startupContentLoadData') }}</p>
+      <v-app-bar-title>{{ $t('startupContentLoadData') }}</v-app-bar-title>
       <v-spacer></v-spacer>
       <v-btn :href="stores.api().returnUrl">
         <v-icon left icon="mdi-logout-variant"></v-icon>
